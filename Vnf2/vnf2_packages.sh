f7d898dac252eacb9d158dc782861a38631aa657
SakHam <36757898+SakHam@users.noreply.github.com>
2020-06-30 14:38:25 +0300

    Create vnf2_packages.sh

---
+++ Vnf2/vnf2_packages.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install zip -y
+sudo apt-get install python -y
+sudo apt-get install python-pip -y
+sudo pip3 install -U Flask
+sudo pip3 install flask-restful
