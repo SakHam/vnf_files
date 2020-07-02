054b9e4a05a91cd2d72624a0f17fc49a0269d734
SakHam <36757898+SakHam@users.noreply.github.com>
2020-07-02 14:51:09 +0300

    Update service_Vnf1.py

--- Vnf1/service_Vnf1.py
+++ Vnf1/service_Vnf1.py
@@ -45,25 +45,30 @@ def start():
   network_url = request.args.get('network_url','')
   object_name = request.args.get('object_name','')  
   PARAMS = {'camera_name':camera_name, 'network_url':network_url, 'object_name':object_name} 
-  
-  URL1 = "http://192.168.79.18:5000/start"
+  chk = 0
+  URL1 = "http://192.168.79.18:5000/start/"
   
   r_object_detect = requests.post(url = URL1, data = PARAMS) 
-  
-  data_obj_det = r_object_detect.text() 
- 
-  URL2 = "http://192.168.79.10:5000/start"
+   
+  URL2 = "http://192.168.79.10:5000/start/"
   r_monitoring = requests.post(url = URL2, data = PARAMS) 
   
-  data_monitoring = r_monitoring.text() 
   
+  if r_object_detect.status_code == 200 :
+    print('The Start Detection Service was send succesfully')  
+    chk = chk + 1
+  else:
+    print('The Start Detection Service failed')
+   if r_monitoring.status_code == 200 :
+    print('The Start Monitoring Service was send succesfully')    
+    chk = chk + 1
+  else:
+    print('The Start Monitoring Service failed')  
   
-  print(data_obj_det)
-  print(data_monitoring)
-  
-  
-  
-  return "Send the start requests : " + data_obj_det + data_monitoring
+  if chk == 2:
+    return "Start requests sent Succesfully"
+  else:
+    return "Start requests failed"
 
 
 
@@ -72,25 +77,32 @@ def start():
 @app.route('/stop/',methods = ['POST'])
 def stop():
   camera_name = request.args.get("camera_name","")
+  chk = 0
+  
   PARAMS = {'camera_name':camera_name} 
   
   URL1 = "http://192.168.79.18:5000/stop"
   r_object_detect = requests.post(url = URL1, data = PARAMS) 
-  
-  data_obj_det = r_object_detect.text() 
- 
+   
   URL2 = "http://192.168.79.10:5000/stop"
   r_monitoring = requests.post(url = URL2, data = PARAMS) 
   
-  data_monitoring = r_monitoring.text() 
+  if r_object_detect.status_code == 200 :
+    print('The Stop Detection Service was send succesfully')  
+    chk = chk + 1
+  else:
+    print('The Stop Detection Service failed')
+   if r_monitoring.status_code == 200 :
+    print('The Stop Monitoring Service was send succesfully')    
+    chk = chk + 1
+  else:
+    print('The Stop Monitoring Service failed')  
   
-  
-  print(data_obj_det)
-  print(data_monitoring)
-  
-  
-  
-  return "Send the stop requests : " + data_obj_det + data_monitoring
+  if chk == 2:
+    return "Stop requests sent Succesfully"
+  else:
+    return "Stop requests failed"  
+
   
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
